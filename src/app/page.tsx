32779e775126b66d23dad6dab8191f1af5525070
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-15 14:37:45 -0300

    Remove HomePage component and associated product fetching logic

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Products from "./components/Products";
-import { getProducts } from "./services";
-
-
-export default  async function HomePage() {
-const products = await getProducts();
-
-  return <Products products={products} />
-}
