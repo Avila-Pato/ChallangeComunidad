4d46ec682d4bafbe01db39c0c4c4d050b060b916
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-15 14:19:57 -0300

    Refactor ProductsPage component to prepare for optional parameters

--- src/app/[[...segments]]/page.tsx
+++ src/app/[[...segments]]/page.tsx
@@ -2,7 +2,7 @@ import Products from "@/app/components/Products";
 import { getProducts } from "../services";
 
 
-
+// esto cambiaria a un optional ? 
 export default async function ProductsPage({ 
     params: {
       segments}}:{
